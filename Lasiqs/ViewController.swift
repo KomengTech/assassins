29e09eb1dd4d4002b82059f562047e7f737c0134
Hafiz <apple@Apples-MacBook-Pro.local>
2017-03-22 20:11:39 +0800

    first commit

---
+++ Lasiqs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lasiqs
+//
+//  Created by Hafiz on 22/3/17.
+//  Copyright © 2017 KomengTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
